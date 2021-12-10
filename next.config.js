0af431261a72fcf6b940ccb70dd22f62c360e40d
Avik <ratnabaidya546@gmail.com>
2021-12-10 21:29:26 +0530

    1st commit

---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    images: {
+      domains: ['links.papareact.com'],
+    },
+      env:{
+        mapbox_key:'pk.eyJ1IjoiYXZpazE4IiwiYSI6ImNreDBobjVibTAyYW4yb3J6ZTVveXpjYTUifQ.9nlK4go44jXbVLjfEDLawA'
+      },
+  }
